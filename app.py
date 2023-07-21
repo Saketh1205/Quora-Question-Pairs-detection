036cc022a68b6492606f5a2ef7de5384499ad864
Saketh1205 <96828011+Saketh1205@users.noreply.github.com>
2023-07-22 01:32:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import helper
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+
+model = keras.models.load_model('bilstm_model.h5')
+
+st.header('Duplicate Question Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+
+if st.button('Find'):
+    query = helper.query_point_creator(q1,q2)
+    result = model.predict(query)[0]
+
+    if result:
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
+
+
+
+
